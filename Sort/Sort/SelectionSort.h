5550443f56a18e317200504e68e96a7ad281c129
진현수 <jhs9253@naver.com>
2018-01-15 01:01:40 +0900

    SelectionSort Add

--- Sort/Sort/SelectionSort.h
+++ Sort/Sort/SelectionSort.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <vector>
+
+void SelectionSort(vector<int> v)
+{
+	for (int i = 0; i < v.size() - 1; i++)
+	{
+		int _temp = 1;
+		for (int j = i + 1; j < v.size(); j++)
+		{
+			if (v[_temp] >= v[j])
+				_temp = j;
+		}
+		swap(v[i], v[_temp]);
+	}
+}
--- Sort/Sort/Sort.cpp
+++ Sort/Sort/Sort.cpp
@@ -1,4 +1,12 @@
+#include "BubbleSort.h"
+#include "SelectionSort.h"
+#include "InsertionSort.h"
+#include "MergeSort.h"
+#include "QuickSort.h"
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 enum class SORT
 {
@@ -11,5 +19,38 @@ enum class SORT
 
 int main()
 {
-	
+	int _iNum = 0;
+	cout << "Input Sort : ";
+	cin >> _iNum;
+
+	switch (_iNum)
+	{
+		case static_cast<int>(SORT::BUBBLESORT):
+		{
+
+		}
+		break;
+		case  static_cast<int>(SORT::SELECTIONSORT):
+		{
+
+		}
+		break;
+		case  static_cast<int>(SORT::INSERTIONSORT):
+		{
+			
+		}
+		break;
+		case  static_cast<int>(SORT::MERGESORT):
+		{
+
+		}
+		break;
+		case  static_cast<int>(SORT::QUICKSORT):
+		{
+
+		}
+		break;
+	default:
+		break;
+	}
 }
